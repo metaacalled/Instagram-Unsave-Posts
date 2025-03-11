71be8551138cdd38c89f5c68e2f08b5369e3956a
metacalled <46867837+metaacalled@users.noreply.github.com>
2025-03-11 10:49:58 +0100

    Now also clicks for posts that need confirmation.

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 function goNext() {
-    document.querySelector('[aria-label="Next"]').parentElement.click();
-    setTimeout(unsave, 2000);
-}
-function unsave() {
     document.querySelector('[aria-label="Remove"]').parentElement.click();
-    setTimeout(goNext, 2000)
+    setTimeout(unsave, 1000);
 }
-setTimeout(unsave, 2000);
+
+function unsave() {
+    let removeButton = document.querySelector('[aria-label="Next"]');
+    if (removeButton) {
+        removeButton.parentElement.click();
+        setTimeout(checkDeleteButton, 1000);
+    } else {
+        console.warn("Remove button not found.");
+        setTimeout(goNext, 1000);
+    }
+}
+
+function checkDeleteButton() {
+    let deleteButton = document.querySelector('button._a9--._ap36._a9-_');
+    if (deleteButton) {
+        deleteButton.click();
+        console.log("Delete button clicked.");
+    }
+    setTimeout(goNext, 1000);
+}
+
+setTimeout(unsave, 1000);
